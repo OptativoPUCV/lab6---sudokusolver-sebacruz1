de1b26d579014c87952f73406712bd7a4189e748
Replit user <>
2023-05-10 03:09:24 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -57,7 +57,7 @@ int is_valid(Node* n)
 
             if (n->sudo[f][c]!=0)
             {
-                if(arrayFila[n->sudo[f][c]]==1)
+                if (arrayFila[n->sudo[f][c]]==1)
                 {
                     return 0;
                 }
@@ -82,7 +82,7 @@ int is_valid(Node* n)
               int j=3*(f%3) + (c%3);
              if (n->sudo[i][j]!=0)
              {
-                 if(arraySub[n->sudo[i][j]]==1)
+                 if (arraySub[n->sudo[i][j]]==1)
                  {
                      return 0;
                  }
@@ -102,16 +102,20 @@ int is_valid(Node* n)
 
 List* get_adj_nodes(Node* n)
 {
-  
   List* list=createList();
 
-  for(int i = 0; i < 9; i++){
-    for(int j = 0; j < 9; j++){
-      if(n->sudo[i][j] == 0 ){
-        for(int aux = 1; aux < 10; aux++){
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < 9; j++)
+    {
+      if (n->sudo[i][j] == 0)
+      {
+        for(int aux = 1; aux < 10; aux++)
+        {
           n->sudo[i][j] = aux;
           Node* nuevoNodo = copy(n);
-          if(is_valid(nuevoNodo) == 1){
+          if (is_valid(nuevoNodo) == 1)
+          {
             pushBack(list,nuevoNodo);
           }
         }
@@ -126,7 +130,7 @@ int is_final(Node* n)
 {
   for (int i = 0; i < 9; i++)
   {
-    for(int j = 0; j < 9; j++)
+    for (int j = 0; j < 9; j++)
     {
       if (n->sudo[i][j] == 0)
         return 0;
@@ -136,17 +140,19 @@ int is_final(Node* n)
 }
 
 Node* DFS(Node* initial, int* cont){
-  Stack * aux = createStack();
+  Stack *aux = createStack();
   push(aux, initial);
 
-  while(is_empty(aux) != 1){
+  while (is_empty(aux) != 1)
+  {
 
-    Node * primero = top(aux);
+    Node *primero = top(aux);
     pop(aux);
-    if(is_final(primero) == 1) return primero;
-    List * lista = get_adj_nodes(primero);
-    Node * nodos = first(lista);
-    while(nodos != NULL){
+    if (is_final(primero) == 1) return primero;
+    List *lista = get_adj_nodes(primero);
+    Node *nodos = first(lista);
+    while (nodos != NULL)
+    {
       push(aux,nodos);
       nodos = next(lista);
     }
